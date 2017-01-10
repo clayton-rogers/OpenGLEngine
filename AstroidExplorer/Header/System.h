96e03970c17f5ded1cbad1bd50ac8b982bc19093
claytonrogers <claytonrogers53@gmail.com>
2017-01-09 22:44:16 -0500

    Added pre and post loop systems functions

--- AstroidExplorer/Header/System.h
+++ AstroidExplorer/Header/System.h
@@ -9,6 +9,8 @@ class System {
 
 	// do nothing by default
 	virtual void internalRunEntity(unsigned int UID) {};
+	virtual void preLoop() {};
+	virtual void postLoop() {};
 
 protected:
 	ComponentBitset mRequiredComponents;
@@ -16,12 +18,13 @@ protected:
 public:
 
 	virtual void runSystem() {
+		preLoop();
 		for (auto& entity : componentManager.getEntities()) {
-
 			if ((entity.second & mRequiredComponents) == mRequiredComponents) {
 				internalRunEntity(entity.first);
 			}
 		}
+		postLoop();
 	}
 
 	virtual ~System() {
