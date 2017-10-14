f826896c63d8832ded3176ec14d65f42e33f9061
claytonrogers <claytonrogers53@gmail.com>
2017-10-14 11:48:12 -0400

    Fixed averager when there is a small number of samples.

--- OpenGLEngine/Header/Averager.h
+++ OpenGLEngine/Header/Averager.h
@@ -6,11 +6,13 @@ template <typename T, int SAMPLES>
 class Averager {
 	std::array<T, SAMPLES> samples;
 	int nextLocation = 0;
+	int num_samples = 0;
 
 public:
 	void push(T sample) {
 		samples[nextLocation] = sample;
 		nextLocation = (nextLocation + 1) % SAMPLES;
+		if (num_samples < SAMPLES) { ++num_samples; }
 	}
 
 	T sample() {
@@ -18,6 +20,6 @@ public:
 		for (auto sample : samples) {
 			sum += sample;
 		}
-		return sum / SAMPLES;
+		return (num_samples == 0) ? T(0) : sum / num_samples;
 	}
 };
