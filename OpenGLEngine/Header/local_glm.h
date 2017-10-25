87f5a577bc7aae9c183ff4fdf71b9dfb2f768f4d
claytonrogers <claytonrogers53@gmail.com>
2017-10-24 23:15:29 -0400

    Made pragma only for MSVC

--- OpenGLEngine/Header/local_glm.h
+++ OpenGLEngine/Header/local_glm.h
@@ -1,11 +1,15 @@
 #pragma once
 
 
+#ifdef _MSC_VER
 #pragma warning(push, 0)
+#endif // _MSC_VER
 #include <glm/glm.hpp>
 #include <glm/gtx/vector_angle.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/vector_angle.hpp>
 #include <glm/gtx/norm.hpp>
+#ifdef _MSC_VER
 #pragma warning (pop)
+#endif // _MSC_VER
